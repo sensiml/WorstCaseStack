30c2424519a6c7a14719a1cfb4d4617fd7456124
Justin Moore <justin.moore@sensiml.com>
2021-04-28 11:59:12 -0700

    Fix parsing for gcc-10 vs 9.3.1

--- WCS.py
+++ WCS.py
@@ -24,23 +24,25 @@ class WorstCaseStackParser(object):
     def __init__(
         self,
         rtl_ext_end=".dfinish",
-        dir=".",  # Working directory
+        work_dir=".",  # Working directory
         su_ext=".su",
         obj_ext=".o",
         manual_ext=".msu",
         read_elf_path="readelf",  # You may need to enter the full path here
         stdout_encoding="utf-8",  # System dependant
+        dot_c_included=False
     ):
-        self.rtl_ext_end = (".dfinish",)
+        self.rtl_ext_end = rtl_ext_end
         self.rtl_ext = None  # e.g. '.c.270r.dfinish'. The number '270' will change with gcc version and is auto-detected by the
         # function find_rtl_ext
-        self.dir = dir  # Working directory
-        self.su_ext = ".su"
-        self.obj_ext = ".o"
-        self.manual_ext = ".msu"
-        self.read_elf_path = "readelf"  # You may need to enter the full path here
-        self.stdout_encoding = "utf-8"  # System dependant
-        self.call_graph = dict()
+        self.work_dir = work_dir  # Working directory
+        self.su_ext = su_ext
+        self.obj_ext = obj_ext
+        self.manual_ext = manual_ext
+        self.read_elf_path = read_elf_path  # You may need to enter the full path here
+        self.stdout_encoding = stdout_encoding  # System dependant
+        self.call_graph = {"locals": {}, "globals": {}, "weak": {}}
+        self.dot_c_included = dot_c_included
 
     def read_symbols(self, file):
         from subprocess import check_output
@@ -59,7 +61,7 @@ class WorstCaseStackParser(object):
                 s2.name = ""
 
             return s2
-
+        print ([self.read_elf_path, "-s", "-W", file])
         output = check_output([self.read_elf_path, "-s", "-W", file]).decode(
             self.stdout_encoding
         )
@@ -72,7 +74,8 @@ class WorstCaseStackParser(object):
         :param tu: name of the translation unit (e.g. for main.c, this would be 'main')
         :param call_graph: a object used to store information about each function, results go here
         """
-        symbols = self.read_symbols(tu[0 : tu.rindex(".")] + self.obj_ext)
+        file = tu if self.dot_c_included else tu[0 : tu.rindex(".")]
+        symbols = self.read_symbols(file + self.obj_ext)
 
         for s in symbols:
 
@@ -170,11 +173,11 @@ class WorstCaseStackParser(object):
         static_call = re.compile(r'^.*\(call.*"(.*)".*$')
         other_call = re.compile(r"^.*call .*$")
 
-        for line_ in open(tu + rtl_ext).readlines():
+        for line_ in open(tu + self.rtl_ext).readlines():
             m = function.match(line_)
             if m:
                 fxn_name = m.group(2)
-                fxn_dict2 = find_fxn(tu, fxn_name, self.call_graph)
+                fxn_dict2 = self.find_fxn(tu, fxn_name)
                 if not fxn_dict2:
                     raise Exception(
                         "Error locating function {} in {}".format(fxn_name, tu)
@@ -204,22 +207,27 @@ class WorstCaseStackParser(object):
         :return:
         """
 
-        # su_line = re.compile(r'^([^ :]+):([\d]+):([\d]+):([a-zA-Z]+)\s([a-zA-Z_]+)\t(\d+)\t(\S+)$')
-        su_line = re.compile(
-            r"([^\n:]+):([\d]+):([\d]+):([\w\d\s\*_]+)\s+([\w\d_]+)\(.*\)\t+(\d+)\t"
+        su_line = re.compile(r'^([^ :]+):([\d]+):([\d]+):(.+)\t(\d+)\t(\S+)$')
+        su_line_alt = re.compile(
+            r"([^\n:]+):([\d]+):([\d]+):([\w\d\s\*_]+)\s+([\w\d_]+)\(.*\)\t+(\d+)\t+"
         )
-        i = 1
-
-        for line in open(tu[0 : tu.rindex(".")] + self.su_ext).readlines():
+        file = tu if self.dot_c_included else tu[0 : tu.rindex(".")]
+        print(file+self.su_ext)
+        for line in open(file + self.su_ext).readlines():
+            print(line)
             m = su_line.match(line)
-            if m:
-                fxn = m.group(5)
-                fxn_dict2 = find_demangled_fxn(tu, fxn, self.call_graph)
-
-                fxn_dict2["local_stack"] = int(m.group(6))
+            if m is None:
+                m = su_line_alt.match(line)
+                if m:
+                    fxn = m.group(5)
+                    fxn_dict2 = self.find_demangled_fxn(tu, fxn)
+                    fxn_dict2["local_stack"] = int(m.group(6))
+                else:
+                    print("error parsing line {} in file {}".format(i, file))
             else:
-                print("error parsing line {} in file {}".format(i, tu))
-            i += 1
+                fxn = m.group(4)
+                fxn_dict2 = self.find_demangled_fxn(tu, fxn)
+                fxn_dict2["local_stack"] = int(m.group(5))
 
     def read_manual(self, file):
         """
@@ -274,7 +282,7 @@ class WorstCaseStackParser(object):
             fxn_dict2["unresolved_calls"] = set()
 
             for call in fxn_dict2["calls"]:
-                call_dict = find_fxn(fxn_dict2["tu"], call, self.call_graph)
+                call_dict = self.find_fxn(fxn_dict2["tu"], call)
                 if call_dict:
                     fxn_dict2["r_calls"].append(call_dict)
                 else:
@@ -403,13 +411,13 @@ class WorstCaseStackParser(object):
 
     def find_rtl_ext(self):
         # Find the rtl_extension
-        global rtl_ext
 
-        for root, directories, filenames in os.walk(self.dir):
+        print (self.work_dir)
+        for root, directories, filenames in os.walk(self.work_dir):
             for f in filenames:
                 if f.endswith(self.rtl_ext_end):
-                    rtl_ext = f[f[: -len(self.rtl_ext_end)].rindex(".") :]
-                    print("rtl_ext = " + rtl_ext)
+                    self.rtl_ext = f[f[: -len(self.rtl_ext_end)].rindex(".") :]
+                    print("rtl_ext = " + self.rtl_ext)
                     return
 
         print(
@@ -425,13 +433,13 @@ class WorstCaseStackParser(object):
         for root, directories, filenames in os.walk("."):
             for filename in filenames:
                 all_files.append(os.path.join(root, filename))
-
-        files = [f for f in all_files if os.path.isfile(f) and f.endswith(rtl_ext)]
+        print(all_files)
+        files = [f for f in all_files if os.path.isfile(f) and f.endswith(self.rtl_ext)]
         for f in files:
-            base = f[0 : -len(rtl_ext)]
+            base = f[0 : -len(self.rtl_ext)]
             short_base = base[0 : base.rindex(".")]
-            base_to_use = base if dot_c_included else short_base
-            print(base_to_use)
+            base_to_use = base if self.dot_c_included else short_base
+            print(base_to_use + self.su_ext + " \t" + base_to_use + self.obj_ext)
             if (
                 base_to_use + self.su_ext in all_files
                 and base_to_use + self.obj_ext in all_files
@@ -439,7 +447,7 @@ class WorstCaseStackParser(object):
                 tu.append(base)
                 print(
                     "Reading: {}{}, {}{}, {}{}".format(
-                        base, rtl_ext, short_base, self.su_ext, short_base, self.obj_ext
+                        base, self.rtl_ext, short_base, self.su_ext, short_base, self.obj_ext
                     )
                 )
 
@@ -476,6 +484,7 @@ def main(argv):
         "-o",
         "--obj_ext",
         dest="obj_ext",
+        default=".o",
         help="Object file extension",
     )
 
@@ -498,24 +507,28 @@ def main(argv):
         "-d",
         "--directory",
         dest="directory",
-        default="readelf",
+        default=".",
         help="Base directory for files. Will be recursively scanned.",
     )
     args = parser.parse_args()
-
+    print(args.obj_ext)
     wcs_parser = WorstCaseStackParser(
+        rtl_ext_end=".dfinish",
+        work_dir=args.directory,
         su_ext=args.su_ext,
         obj_ext=args.obj_ext,
+        manual_ext=".msu",
         read_elf_path=args.read_elf_path,
-        dir=args.directory,
+        stdout_encoding="utf-8",
+        dot_c_included = args.dot_c_included
     )
 
     # Find the appropriate RTL extension
     wcs_parser.find_rtl_ext()
 
     # Find all input files
-    call_graph = {"locals": {}, "globals": {}, "weak": {}}
-    tu_list, manual_list = find_files(args.dot_c_included)
+
+    tu_list, manual_list = wcs_parser.find_files()
 
     # Read the input files
     for tu in tu_list:
@@ -526,13 +539,13 @@ def main(argv):
             wcs_parser.call_graph["globals"][fxn["name"]] = fxn
 
     for tu in tu_list:
-        wcs_parser.read_rtl(tu, call_graph)
+        wcs_parser.read_rtl(tu)
     for tu in tu_list:
-        wcs_parser.read_su(tu, call_graph)
+        wcs_parser.read_su(tu)
 
     # Read manual files
     for m in manual_list:
-        wcs_parser.read_manual(m, call_graph)
+        wcs_parser.read_manual(m)
 
     # Validate Data
     wcs_parser.validate_all_data()
